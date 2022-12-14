dd4dcbfd142593a67a757aa0501ecb6bf378840d
EmilianoJO <115809049+EmilianoJO@users.noreply.github.com>
2022-12-14 12:30:09 -0600

    Update models.py

--- assesment1/models.py
+++ assesment1/models.py
@@ -1,9 +1,13 @@
 from odoo import http
 from odoo.http import request
-
+#Sé que hay muchos errores pero hay cosas que no entendí cómo funcionan en odoo e hice sólo la estructura de la parte lógica
 class usuario(http.request):
+	#En la ruta no pude encontrar dónde se guardan los contactos
 	@http.route([‘/contactsdata/’], type=”http”, auth=”public”)
 	def createContact(self, **kw):
+		for key,value in kw:
+			#Con contacts a la información de los contactos pero no se en que formato estaría, si fuera como un objeto creo que funcionaría así, igual en los otros métodos
+			Contacts.key.value=kw.key.value
 		return{
 			'success': True,
 			'status': 'OK',
@@ -40,4 +44,4 @@ class usuario(http.request):
                          'code': 400
                  }
 	def deleteContact(self, **kw):
-                Contacts = [ele for ele in Contacts if ele != kw.value]
+                Contacts = [ele for ele in Contacts if ele.key.value != kw.key.value]
